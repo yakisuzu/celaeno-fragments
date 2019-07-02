f4bf54af2607c369d6037da62c3b61e70ef45425
yakisuzu <yakisuzu@gmail.com>
2019-07-02 17:52:15 +0900

    うごいた

--- ops/gke_create_secret.sh
+++ ops/gke_create_secret.sh
@@ -6,6 +6,7 @@ CLOUDSDK_COMPUTE_ZONE="asia-southeast1-a"
 CLOUDSDK_COMPUTE_REGION="asia-southeast1"
 CLUSTER_NAME="$2"
 GCLOUD_SERVICE_KEY_ENCODED="$3"
+GCR_SERVICE_ACCOUNT="circleci@${PROJECT_ID}.iam.gserviceaccount.com"
 [ "${PROJECT_ID}" == "" ] && echo "Require. PROJECT_ID" && popd > /dev/null && exit 1
 [ "${CLUSTER_NAME}" == "" ] && echo "Require. CLUSTER_NAME" && popd > /dev/null && exit 1
 [ "${GCLOUD_SERVICE_KEY_ENCODED}" == "" ] && echo "Require. GCLOUD_SERVICE_KEY_ENCODED" && popd > /dev/null && exit 1
@@ -15,18 +16,18 @@ GCLOUD_SERVICE_KEY_ENCODED="$3"
 gcloud auth login
 gcloud config configurations list
 
-## kubectl setup
+## kubectl to gcp
 gcloud container clusters get-credentials ${CLUSTER_NAME}
-### local
+### kubectl to local
 kubectl config use-context docker-for-desktop
-### gcp
-#kubectl config use-context ${PROJECT_ID}
 
-# TODO create gcr secret
+# create gcr secret
 kubectl create secret docker-registry gcr-secret \
-  --docker-server=https://asia.gcr.io \
-  --docker-username=_json_key \
-  --docker-password=$(echo ${GCLOUD_SERVICE_KEY_ENCODED} | base64 -di) \
-  --docker-email=circleci@celaeno-fragments.iam.gserviceaccount.com
+  --docker-server="https://asia.gcr.io" \
+  --docker-username="_json_key" \
+  --docker-password="$(echo ${GCLOUD_SERVICE_KEY_ENCODED} | base64 -di)" \
+  --docker-email="${GCR_SERVICE_ACCOUNT}"
+
+kubectl describe secrets gcr-secret
 
 popd > /dev/null
