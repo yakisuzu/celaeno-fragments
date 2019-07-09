07c290f92f7a962385b916154bdc1ed1cf571ca6
yakisuzu <yakisuzu@gmail.com>
2019-07-10 00:37:11 +0900

    shなおした

--- ops/k8s/overlays/local/build.sh
+++ ops/k8s/overlays/local/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-pushd $(dirname `greadlink -f $0`) > /dev/null
+cd `dirname $0`
 
 kustomize build . > ../../manifest/local-k8s.yaml
-
-popd > /dev/null
--- ops/k8s/overlays/production/build.sh
+++ ops/k8s/overlays/production/build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-pushd $(dirname `greadlink -f $0`) > /dev/null
+cd `dirname $0`
 
 IMAGE_NAME="$1"
-[ "${IMAGE_NAME}" == "" ] && echo "Require. IMAGE_NAME" && popd > /dev/null && exit 1
+[ "${IMAGE_NAME}" == "" ] && echo "Require. IMAGE_NAME" && exit 1
 
 kustomize edit set image app=${IMAGE_NAME}
 kustomize build . > ../../manifest/prd-k8s.yaml
-
-popd > /dev/null
