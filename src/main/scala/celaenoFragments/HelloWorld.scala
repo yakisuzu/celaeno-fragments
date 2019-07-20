20fcd2ebdeb7b63348355c6081f06ecb913b2882
yakisuzu <yakisuzu@gmail.com>
2019-07-21 02:18:50 +0900

    cseの結果とれた

--- src/main/scala/celaenoFragments/HelloWorld.scala
+++ src/main/scala/celaenoFragments/HelloWorld.scala
@@ -1,15 +1,20 @@
 package celaenoFragments
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.customsearch.{Customsearch, CustomsearchRequestInitializer}
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object HelloWorld extends App with LazyLogging {
-  lazy val conf: Config   = ConfigFactory.load()
-  lazy val cseApi: String = conf.getString("app.cse.api")
-  lazy val cseCx: String  = conf.getString("app.cse.cx")
+  lazy val conf: Config    = ConfigFactory.load()
+  lazy val cseName: String = conf.getString("app.cse.name")
+  lazy val cseApi: String  = conf.getString("app.cse.api")
+  lazy val cseCx: String   = conf.getString("app.cse.cx")
 
   /*
   scala.util.Properties.envOrElse("TWITTER_CONSUMER_TOKEN_KEY", "fuck")
@@ -42,6 +47,20 @@ object HelloWorld extends App with LazyLogging {
     val trends = ratedData.data
     trends.foreach(trend => trend.trends.foreach(t => logger.info(t.name)))
   }(global)
+
+  lazy val cseClient = new Customsearch.Builder(
+    GoogleNetHttpTransport.newTrustedTransport(),
+    JacksonFactory.getDefaultInstance,
+    null
+  ).setApplicationName(cseName)
+    .setCustomsearchRequestInitializer(new CustomsearchRequestInitializer(cseApi))
+    .build()
+    .cse()
+
+  val results = cseClient.list("cars").setCx(cseCx).execute().getItems.asScala
+  results.foreach { r =>
+    logger.info(r.toString)
+  }
 
   // TODO 本来は終了しないけど、処理ができるまでいったんsleepし続けるように
   Thread.sleep(24.hours.toMillis)
